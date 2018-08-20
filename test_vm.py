b81eef084a5d2f40c22043d56aae277c47502ce1
최 지영 <jiyoung031@gmail.com>
2018-08-20 12:07:26 +0900

    add output beverage

--- test_vm.py
+++ test_vm.py
@@ -33,3 +33,19 @@ def unknow_output():
 
 # 각 테스트는 최소한 겹치지 않도록 만드는 게 가장 중요합니다.
 # 테스트는 실행 순서와 결과와 상관 없이 테스트가 서로 간섭을 받지 않아야합니다. - init()
+
+def test_short():
+    m = VendingMachine()
+    m.run("동전 100")
+    assert "잔액이 부족합니다." == m.run("음료 커피")
+
+def test_enough():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "커피가 나왔습니다." == m.run("음료 커피")
+    assert "잔액은 350원입니다." == m.run("잔액")
+
+def test_unknown_drink():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "알 수 없는 음료입니다." == m.run("음료 맥주")
--- vm.py
+++ vm.py
@@ -3,12 +3,18 @@
 # class에 def 함수를 쓰면 메서드가 됩니다.
 # 메서드는 self 인자(receiver, 여기서는 test_vm.py의 m)를 항상 받아야합니다.
 
+
 class VendingMachine:
     def __init__(self):
         self._change = 0
 
+# def __init__이 "상태"를 의미함.
+# 클래스에는 상태와 메서드를 정의한다. 인스턴스가 만들어지면 상태가 생기고,
+# _를 넣는다는 건 건드리지말라는 의미.
+# run은 메소드
 
     def run(self, raw):
+
         tokens = raw.split(" ")
         cmd, params = tokens[0], tokens[1:]
 
@@ -19,5 +25,22 @@ class VendingMachine:
             self._change += int(coin)
             # change = change + int(coin)과 같은 말.
             return coin + "원을 넣었습니다."
+        # elif cmd == "음료 커피":
+        #     return "잔액이 부족합니다."
+        elif cmd == "음료":
+            price = 150
+            unknown_beverage = params[0]
+            if unknown_beverage != "커피":
+                return "알 수 없는 음료입니다."
+            if self._change < price:
+                return "잔액이 부족합니다."
+            self._change = self._change - price
+            return unknown_beverage + "가 나왔습니다."
         else:
             return "알 수 없는 명령어입니다."
+#
+# v1 = VendingMachine()
+# v2 = VendingMachine()
+# v1.run("동전 100")
+
+# v1.run("동전 100")을 실행하면 self에 vi이 담기고 raw에 "동전 100"이 담긴다.
